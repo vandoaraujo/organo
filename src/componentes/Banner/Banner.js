ac780fb07998944db1e2a4c88ad8f7a91592ce27
Vando <vando.araujo@serpro.gov.br>
2025-03-26 16:40:09 -0300

    correcao de imagens

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -4,7 +4,7 @@ export const Banner = () => {
         //react permite uso de fragment, utilizar essa tag no caso de necessitar de mais de um componente html
         <>
         <header className="banner">
-            <img src="/imagens/Banner.png" alt="O Banner principal da pagina do Organo" />
+            <img src="/imagens/banner.png" alt="O Banner principal da pagina do Organo" />
         </header>
         <h1 style={{textAlign: 'center'}}>Sistema de Cadastro</h1>
         </>
--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -5,9 +5,9 @@ const Rodape = () => {
         <footer className='footer'>
             <section>
                 <div>
-                    <a href='http://facebook.com'><img src='/images/fb.png' alt='Facebook logo' /></a>
-                    <a href='http://twitter.com'>< img src='/images/tw.png' alt='Twitter logo' /></a>
-                    <a href='http://instagram.com/'><img src='/images/ig.png' alt='Instagram logo' /></a>
+                    <a href='http://facebook.com'><img src='/imagens/fb.png' alt='Facebook logo' /></a>
+                    <a href='http://twitter.com'>< img src='/imagens/tw.png' alt='Twitter logo' /></a>
+                    <a href='http://instagram.com/'><img src='/imagens/ig.png' alt='Instagram logo' /></a>
                 </div>
                 <img src='/images/logo.png' alt='Logo organo' />
                 <h3>Desenvolvido por Vando</h3>
